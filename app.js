d3bedc043c298ecf2d757aa6e8d720f15f7ca553
BuildTools <unconfigured@null.spigotmc.org>
2020-05-08 23:26:27 +0900

    arranged router codes and added parse query function

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import controls from "./controls/index.js";
 
+function changeURL(path) {
+  window.history.pushState({}, "", path); // change window location object and url
+}
+
+function parseQuerystring(params) {
+  var search = window.location.search;
+  if (search === "") return;
+
+  var qs = search.slice(1).split("&");
+  params.queries = qs.map(function(q) {
+    var pair = q.split("=");
+    var key = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair[1]);
+    return { key, value };
+  });
+}
+
 // check if  path and one of routes match
 function isURLmatched(urlFragments, pathFragments, params) {
   return urlFragments.every(function(fragment, i) {
@@ -12,29 +29,41 @@ function isURLmatched(urlFragments, pathFragments, params) {
   });
 }
 
-// parse URL and execute matched conftroller
-function router(path) {
-  var urls = Object.keys(routes);
-  var r = null;
-  var match = false;
-  var params = {};
-
+function findMatchedURL(urls, params) {
+  var matchedURL = "";
   urls.some(function(url) {
-    var pathFragments = path.split("/");
+    var pathFragments = window.location.pathname.split("/");
     var urlFragments = url.split("/");
     if (pathFragments.length !== urlFragments.length) return;
 
     if (isURLmatched(urlFragments, pathFragments, params)) {
-      r = routes[url]; // store matched controller
+      matchedURL = url;
       return;
     }
   });
+  return matchedURL;
+}
 
+function routeToMatchedURL(urls, params) {
+  var r = null;
+  var matchedURL = findMatchedURL(urls, params);
+  r = routes[matchedURL]; // store matched controller
   (r || routes.otherwise)(params); // execute controller
+}
 
+// path는 다음에 이동할 URL 주소이므로 history pushState함수로 location 객체를 다음 URL 상태로 변경해줘야
+// 다음 URL의 parameter나 querystring을 가져올 수 있으므로 history pushState 함수는 상단에 위치하였음
+
+// parse URL and execute matched conftroller
+function router(path) {
+  var urls = Object.keys(routes);
+  var params = {};
+
+  // don't change order of code
+  changeURL(path);
+  parseQuerystring(params);
+  routeToMatchedURL(urls, params);
   console.log(params);
-
-  // (routes[path] || routes.otherwise)(path);
 }
 
 var routes = {
@@ -55,8 +84,7 @@ var routes = {
   }
 };
 
-var path = window.location.pathname;
-router(path);
+router("/");
 
 // navigate forwards or backwards from url history
 window.addEventListener("popstate", function(e) {
--- components/nav.js
+++ components/nav.js
@@ -10,7 +10,7 @@ var nav = (function() {
     return `<button data-url="/">home</button>
                 <button data-url="/${nav_data.urls[0]}">${nav_data.urls[0]}</button>
                 <button data-url="/${nav_data.urls[1]}">${nav_data.urls[1]}</button>
-                <button data-url="/${nav_data.urls[2]}/9">${nav_data.urls[2]}</button>
+                <button data-url="/${nav_data.urls[2]}/9?name=computer&price=$30">${nav_data.urls[2]}</button>
                 <button data-url="/${nav_data.urls[3]}">${nav_data.urls[3]}</button>
               `;
   }
