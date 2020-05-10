deb99cc2cd43f145bc22463a0a04fe7623bd82fe
BuildTools <unconfigured@null.spigotmc.org>
2020-05-10 16:26:44 +0900

    enhanced deepcopy functionality of object

--- controls/homeCtrl.js
+++ controls/homeCtrl.js
@@ -2,6 +2,38 @@ import pages from "../pages/index.js";
 import components from "../components/index.js";
 import lib from "../lib/index.js";
 
+function testDeepCopy() {
+  var target = {};
+  var source = {
+    name: "sylee",
+    age: 36,
+    fruit: {
+      name: "apple",
+      price: { dallar: "$ 30", krw: "32000 won" }
+    },
+    school: undefined,
+    list: [
+      { item: "banana", delicious: true },
+      { item: "orange", delicious: false },
+      [1, { inside: 80 }]
+    ]
+  };
+  lib.utils.deepCopyObj(target, source);
+  // 객체의 속성이 배열이고 해당 배열의 요소가 배열인 경우
+  // 배열 자체는 깊은 복사를 하지만 내부에 객체가 있으면 얕은복사가 된다
+  target.list[2][1].inside = "computer";
+  console.log(source.list[2][1].inside, target.list[2][1].inside);
+  // 객체의 속성이 배열이고 해당 배열의 요소가 객체인 경우
+  // 객체는 깊은복사가 된다
+  source.list[1].item = "book";
+  console.log(source.list[1].item, target.list[1].item);
+  // 객체의 속성이 객체이고 해당 객체 내부에 객체가 있는 경우
+  // 재귀함수에 의해 깊은복사가 된다
+  target.fruit.price.krw = "700원";
+  console.log(source.fruit.price.krw, target.fruit.price.krw);
+  return target;
+}
+
 // router 모듈 함수형 프로그래밍 적용하기
 //  컨트롤러 좀 더 정리하기 (init 함수 필요한지 생각해보기 )
 // filter map 함수 사용하기
@@ -70,27 +102,14 @@ var home = (function() {
     return dom;
   }
 
-  // function testDeepCopy() {
-  //   var target = {};
-  //   var source = {
-  //     name: "sylee",
-  //     age: 36,
-  //     fruit: {
-  //       name: "apple",
-  //       price: { dallar: "$ 30", krw: "32000 won" }
-  //     }
-  //   };
-  //   lib.utils.deepCopyObj(target, source);
-  //   return target;
-  // }
-
   // dictate all of handlers for page (Controller)
   function attachHandler(dom) {
     console.log("homepage handler attached !");
 
     dom.nav.addEventListener("click", function(e) {
       lib.router(e.target.dataset.url);
-      // console.log(testDeepCopy());
+      testDeepCopy();
+
       // console.log(lib.utils.generateUUID4());
     });
   }
