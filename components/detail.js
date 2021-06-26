a2f7609a8f04f05c6b7049396f2291a697cd1968
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 01:59:21 +0900

    changed style of detail page

--- components/detail.js
+++ components/detail.js
@@ -1,13 +1,13 @@
-import lib from "../lib/index.js";
-import defaultMovieCover from "../resources/default-movie.png";
-import downArrow from "../resources/down-arrow.png";
+import lib from "../lib/index.js"
+import defaultMovieCover from "../resources/default-movie.png"
+import downArrow from "../resources/down-arrow.png"
 
 // item 컴포넌트는 각각이 서로 다른 렉시컬 실행환경을 가져야 하므로 즉시실행함수로 한번만 실행하면 하나의 실행환경만 가지게 됨
 // 여러개의 서로 다른 실행환경을 가지려면 item 을 클로저로 만들어서 실행환경을 만들고 싶을때마다 함수를 실행해야 함
 // 쉽게 말해서 아래 함수가 실행될때마다(리턴할때마다) 메모리에 서로 다른 실행환경이 만들어지고 각각의 실행환경들은 메모리 변수에 서로 다른 값들을 가지게 된다
 // 즉 각각의 item 은 메모리에 서로 다른 값들을 저장하고 서로 다르게 업데이트가 가능하다
 var detail = (function() {
-  "use strict";
+  "use strict"
 
   var detail_data = {
     id: 0,
@@ -21,7 +21,7 @@ var detail = (function() {
     genres: "",
     trailer: "",
     torrentUrl: ""
-  };
+  }
 
   //  아이템을 클릭했을때 image를 클릭하느냐 li 배경을 클릭하느냐 title을 클릭하느냐에 따라 e.target이 달라져서 그냥 모든 요소에 같은 id값을 삽입했음
   //  css 는 폴더 하나 만들고 컴포넌트마다 css 분리해서 관리하고 인라인 스타일 대신 class 선언해서 스타일링하기
@@ -32,14 +32,17 @@ var detail = (function() {
     return `<div id=${detail_data.id} class="detail-card">
                   <div class="detail-main">
                     <div class="detail-info">
-                      <img src="${detail_data.cover}" class="detail-cover"/>
-                      <h4 class="detail-title">${detail_data.title} (${
+                     
+                        <img src="${detail_data.cover}" class="detail-cover"/>
+                    
+                      <div class="detail-description">
+                        <h4 class="detail-title">${detail_data.title} (${
       detail_data.rating
     }) </h4>
-                      <h4 class="detail-title">${detail_data.year} (${
+                        <h4 class="detail-title">${detail_data.year} (${
       detail_data.runtime
     } min.)</h4>
-                      
+                      <div class="detail-trailer-container">
                        <a href="${
                          detail_data.trailer
                            ? `https://www.youtube.com/watch?v=${detail_data.trailer}`
@@ -47,13 +50,17 @@ var detail = (function() {
                        }" target="_blank" class="detail-trailer">${
       detail_data.trailer ? "Click to see trailer" : "No trailer"
     }</a>
+    </div>
+                </div>
                       <div class="detail-down-arrow">
                         <img id="detail-down-arrow" src="${detail_data.arrow}"/>
                       </div>
                     </div>
 
                     <div id="detail-summary" class="detail-summary">
-                      ${detail_data.summary} <br/>
+                      <div class="detail-summary-contents">${
+                        detail_data.summary
+                      }</div> <br/>
                       
                       <a href="${
                         detail_data.torrentUrl ? detail_data.torrentUrl : ""
@@ -67,21 +74,21 @@ var detail = (function() {
                     #${detail_data.genres}
                   </div>
 
-                </div>`;
+                </div>`
   }
 
   // not functional => change obj inside function
   function updateData(updateObj) {
-    lib.dom.updateObj(detail_data, updateObj);
+    lib.dom.updateObj(detail_data, updateObj)
   }
   function getData() {
-    return detail_data;
+    return detail_data
   }
   return {
     getTemplate,
     updateData,
     getData
-  };
-})();
+  }
+})()
 
-export default detail;
+export default detail
--- controls/aboutCtrl.js
+++ controls/aboutCtrl.js
@@ -1,30 +1,30 @@
-import pages from "../pages/index.js";
-import components from "../components/index.js";
-import lib from "../lib/index.js";
+import pages from "../pages/index.js"
+import components from "../components/index.js"
+import lib from "../lib/index.js"
 
 var about = (function() {
-  "use strict";
+  "use strict"
 
-  var about_data = {};
+  var about_data = {}
 
   function init(params) {
-    var data = { params };
+    var data = { params }
 
     about_data.domIDs = {
       root: "root",
       nav: "nav",
       detail: "detail"
-    };
+    }
 
     about_data.sessionStorageIDs = {
       movies: "movies"
-    };
+    }
 
-    about_data.params = params; // 련재 페이지에서 비동기 업데이트시 params를 사용하기 위함
+    about_data.params = params // 련재 페이지에서 비동기 업데이트시 params를 사용하기 위함
     about_data.movies =
       JSON.parse(sessionStorage.getItem(about_data.sessionStorageIDs.movies)) ||
-      [];
-    return data;
+      []
+    return data
   }
 
   // fetch data from server, REST API, sessionStorage, URL parameters, URL querystring (Model)
@@ -35,83 +35,85 @@ var about = (function() {
     // 클로저는 메모리에 저장하기 때문에 새로고침하면 저장된 데이티가 초기화됨
 
     if (initData.params.id) {
-      console.log(about_data.movies);
+      console.log(about_data.movies)
       var clicked_movie = about_data.movies.filter(function(movie) {
-        return movie.id === parseInt(initData.params.id);
-      });
-      components.detail.updateData(clicked_movie[0]);
+        return movie.id === parseInt(initData.params.id)
+      })
+      components.detail.updateData(clicked_movie[0])
 
       // 한번이라도 상세 페이지에 들어온 경우 현재 상세 페이지의 id 를 저장했다가 다른 페이지에서 다시 about 버튼을 클릭하면 최근에 본 상세페이지로 이동함
-      sessionStorage.setItem("movie_id", initData.params.id);
+      sessionStorage.setItem("movie_id", initData.params.id)
     }
     components.nav.updateData({
       wishBtnDisplay: false
-    });
+    })
 
-    return initData;
+    return initData
   }
 
   // render to root element and to parent element (View)
   function renderAll(data) {
-    var doms = {};
+    var doms = {}
 
     doms[about_data.domIDs.root] = lib.dom.render(
       about_data.domIDs.root,
       pages.about.getTemplate()
-    ); // root must be the first
+    ) // root must be the first
     doms[about_data.domIDs.nav] = lib.dom.render(
       about_data.domIDs.nav,
       components.nav.getTemplate()
-    );
+    )
     doms[about_data.domIDs.detail] = lib.dom.render(
       about_data.domIDs.detail,
       data.params.id
         ? components.detail.getTemplate()
         : "Theres is no movie chosen. please choose movie first :)"
-    ); // root must be the first
+    ) // root must be the first
 
-    return doms;
+    return doms
   }
 
   // dictate all of handlers for page (Controller)
   function attachHandler(doms) {
-    console.log("aboutpage handler attached !");
+    console.log("aboutpage handler attached !")
 
     doms.nav.addEventListener("click", function(e) {
-      var routingUrl = e.target.dataset.url;
-      var movieId = sessionStorage.getItem("movie_id");
+      var routingUrl = e.target.dataset.url
+      var movieId = sessionStorage.getItem("movie_id")
       if (routingUrl) {
         // 만약 이전에 상세 페이지에 한번이라도 들어왔다면 about 버튼 클릭시 최근 상세페이지로 이동하도록 navigation url을 변경함
         // 물론 모든 페이지들이 클로저라서 상세페이지에 한번이라도 들어왔다면 메모리에 저장된 데이터로 최근 상세페이지를 보여주지만
         // 만약에 홈페이지를 새로고침하게 되면 최근 상세페이지 데이터들이 사라지므로 about 버튼으로 다시 들어가면 초기화되었음
         if (routingUrl === "/about" && movieId) {
-          routingUrl = `/about/${movieId}`;
+          routingUrl = `/about/${movieId}`
         }
-        lib.router(routingUrl);
+        lib.router(routingUrl)
       }
-    });
+    })
 
     doms.detail.addEventListener("click", function(e) {
       if (e.target.id === "detail-down-arrow") {
-        console.log("see summury !!");
-        var summuryEl = doms.detail.querySelector("#detail-summary");
-        summuryEl.classList.toggle("show-summury");
-        console.log(summuryEl);
+        console.log("see summury !!")
+        var summuryEl = doms.detail.querySelector("#detail-summary")
+        var infoEl = doms.detail.querySelector(".detail-info")
+        summuryEl.classList.toggle("show-summury")
+        infoEl.classList.toggle("hide-info")
+        console.log(summuryEl)
       }
-    });
+    })
   }
 
   // pay attention to orders of methods
   // 스코프 체인에 의해 attachHandler 메서드는 외부에서 router를 찾음
   function control(params) {
-    var initData = init(params);
-    var data = getData(initData);
-    var doms = renderAll(data);
-    attachHandler(doms);
+    var initData = init(params)
+    var data = getData(initData)
+    var doms = renderAll(data)
+    attachHandler(doms)
   }
   return {
     control
-  };
-})();
+  }
+})()
 
-export default about;
+export default about
